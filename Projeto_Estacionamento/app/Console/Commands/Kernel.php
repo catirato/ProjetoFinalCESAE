e74b785fd0c15da09ceeada2076cd14c58499246
Sara Carvalho <saracfcarvalho96@gmail.com>
2026-01-31 18:36:51 +0000

    Ajustes reset mensal de pontos

--- Projeto_Estacionamento/app/Console/Commands/Kernel.php
+++ Projeto_Estacionamento/app/Console/Commands/Kernel.php
@@ -8,20 +8,23 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
-        // Aqui registamos o comando que fizeste
         \App\Console\Commands\CheckReservations::class,
+        \App\Console\Commands\ResetMonthlyPoints::class, 
     ];
 
     protected function schedule(Schedule $schedule)
     {
+        // Regra das 10h30
         $schedule->command('check:reservations')->dailyAt('10:31');
+
+        // Reset mensal de pontos (último dia do mês)
+        $schedule->command('points:reset')->monthlyOn(28, '23:59');
+        //  28 funciona SEMPRE (Laravel ajusta para último dia)
     }
 
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
+        require base_path('routes/console.php');
     }
 }
-
-
-//Schedule da regra das 10h30
