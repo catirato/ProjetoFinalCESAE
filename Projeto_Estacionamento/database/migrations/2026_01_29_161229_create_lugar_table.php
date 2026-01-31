e4c5900b821c0ab5f98b9fb0b3819f4d6294d9a8
Sara Carvalho <saracfcarvalho96@gmail.com>
2026-01-31 14:31:07 +0000

    tables migrations

--- Projeto_Estacionamento/database/migrations/2026_01_29_161229_create_lugar_table.php
+++ Projeto_Estacionamento/database/migrations/2026_01_29_161229_create_lugar_table.php
@@ -6,22 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-       Schema::create('lugar', function (Blueprint $table) {
+        Schema::create('lugar', function (Blueprint $table) {
             $table->id();
             $table->integer('numero')->unique();
             $table->boolean('ativo')->default(true);
         });
-
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('lugar');
