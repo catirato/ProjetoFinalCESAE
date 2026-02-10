b738e929b0df76b730e61bf72a92103645a463ea
jandirapedrosa <jandira.mpedrosa@gmail.com>
2026-02-10 00:21:54 +0000

    criação da homepage

---
+++ Projeto_Estacionamento/resources/views/homepage.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('content')
+
+  {{-- <x-slot:title>
+    Welcome
+  </x-slot:title> --}}
+
+    <div class="max-w-2xl mx-auto">
+      <div class="card bg-base-100 shadow mt-8">
+        <div class="card-body">
+          <div>
+            <h1 class="text-3xl font-bold">Seja bem vindo!</h1>
+            <p class="mt-4 text-base-content/60">Esta é a nova aplicação de estacionameto do CESAE Digital</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+@endsection
--- Projeto_Estacionamento/routes/web.php
+++ Projeto_Estacionamento/routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// home page
+Route::get('/home', function () {
+    return view('homepage');
+});
+
+// registration route
+Route::view('/register', 'login')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
